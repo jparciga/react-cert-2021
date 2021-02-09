05f8597daf6b8ba39b4ee0fbe231928f48302162
José Arciga <jp.arciga@gmail.com>
2021-02-09 17:57:24 -0600

    initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./components/App";
-import "./global.css";
+import App from './components/App';
+import './global.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Hi!</h1>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
